ec9a4473ff6c72621042c4b92ecc3db4e804273b
matano-maho <matano.maho@icloud.com>
2024-05-08 07:55:24 +0000

    lk

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="タイトル", layout="wide")
 st.title('MBI計算')
 
 user_input = st.number_input('あなたの体重を入力してください',min_value=1)
-user_input2 = st.number_input("あなたの身長を入力してください(単位ｍ)"min_value=0.1)
+user_input2 = st.number_input("あなたの身長を入力してください(単位ｍ)",min_value=0.1)
 
 BMI = (user_input / user_input2 / user_input2)
 

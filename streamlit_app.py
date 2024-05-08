b6766a6478c567e777a97e62c671bc95096a53a9
matano-maho <matano.maho@icloud.com>
2024-05-08 08:03:40 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,13 @@ st.set_page_config(page_title="タイトル", layout="wide")
 # タイトルを設定
 st.title('MBI計算')
 
+if st.button("計算"):
+  st.write = ("あなたのBPMは"+str(BMI)+"です。")
+
 user_input = st.number_input('あなたの体重を入力してください',min_value=1)
 user_input2 = st.number_input("あなたの身長を入力してください(単位ｍ)",min_value=0.1)
 
 BMI = (user_input / user_input2 / user_input2)
 
+st.button = ("計算")
 st.write = ("あなたのBPMは"+str(BMI)+"です。")

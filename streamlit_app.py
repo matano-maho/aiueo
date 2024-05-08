322a5ec28534374f25fcf32edfc22bd32b35033c
matano-maho <matano.maho@icloud.com>
2024-05-08 07:53:06 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,6 @@ st.title('MBI計算')
 user_input = st.number_input('あなたの体重を入力してください')
 user_input2 = st.number_input("あなたの身長を入力してください(単位ｍ)")
 
-BPM = user_input/user_input2/user_input2
+BPM = (user_input/user_input2/user_input2)
 
 st.write = ("あなたのBPMは"+str(BPM)+"です。")

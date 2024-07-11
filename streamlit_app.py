b69fb4382f54ea05651628a32d86b798a400a4e2
matano-maho <matano.maho@icloud.com>
2024-07-11 07:58:32 +0000

    ui;t

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,4 +62,6 @@ if 'selected_word' in st.session_state:
     if st.session_state.display_meaning:
         st.write(f"意味: {st.session_state.selected_word['意味']}")
 
+korowaza = selected_word
 
+print(korowaza)
